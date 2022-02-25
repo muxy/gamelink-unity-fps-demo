881968a268ecdb6c98997d31aabd1dc760d6d85f
Troy <tshanks@muxy.io>
2022-02-25 12:10:20 -0500

    Added gitignore, renamed Extension folder, and updated state with relevant info

---
+++ Extension/src/shared/hooks/use-state.js
@@ -0,0 +1,50 @@
+import { inject, provide, reactive } from "vue";
+
+const StateInjectionKey = Symbol("state");
+
+export function provideState(medkit) {
+  const state = reactive(channelStateFromNetwork());
+
+  provide(StateInjectionKey, state);
+
+  return new Promise(resolve => {
+    medkit.loaded().then(() => {
+      medkit.getChannelState().then(st => {
+        try {
+          Object.assign(state, channelStateFromNetwork(st));
+        } catch (err) {
+          console.error(err);
+        }
+
+        // Certain events will be broadcast automatically in response to
+        // server actions. This event is sent whenever the channel state
+        // is changed by the broadcaster.
+        medkit.listen("channel_state_update", st => {
+          try {
+            Object.assign(state, channelStateFromNetwork(st));
+          } catch (err) {
+            console.error(err);
+          }
+        });
+
+        resolve(state);
+      });
+    });
+  });
+}
+
+export function useState() {
+  const state = inject(StateInjectionKey);
+  if (!state) {
+    throw new Error("State could not be found");
+  }
+
+  return { state };
+}
+
+export function channelStateFromNetwork(data) {
+  return {
+    hoverBotsKilled: data?.hoverBotsKilled || 0,
+    turretsKilled: data?.turretsKilled || 0
+  };
+}
